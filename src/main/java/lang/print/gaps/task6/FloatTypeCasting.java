91a0bd51a48521a817b4ffe72cba536147b4cddb
zarina <zarinakamalova117@gmail.com>
2023-03-09 15:07:01 +0600

    done

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        int roundedNumber = Math.round(numberToBeRounded);
+        System.out.println( roundedNumber);
+        }
+    public static void main(String[] args) {
+        FloatTypeCasting ftc = new FloatTypeCasting();
+        ftc.roundNumber(3.6f);
+
     }
 }
--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task6;
 
 public class IntegerTypeCasting {
     public static void main(String[] args) {
+        long first=1000;
+        int second=(int)first;
+        System.out.println(first);
+        System.out.println(second);
     }
+
 }
